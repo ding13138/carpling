05dc73f9fcdc4ba55ae01ca33e7dd400e7a38b67
ulysses22903 <serica.wx508@gmail.com>
2025-05-12 14:40:31 +0900

    Create class_lib.py

---
+++ class_lib.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+class Py24db:
+    def __init__(self):
+        self.__host = "localhost" # ホスト名
+        self.__user = "py24user" # ﾕｰｻﾞｰ名
+        self.__passwd = "py24pass" # パスワード
+        self.__db = "py24db" # DB名
+        self.__reccnt = 0 # レコード数
+        self.__con = mysql.connector.connect(
+        host = self.__host,
+        user = self.__user,
+        passwd = self.__passwd,
+        db = self.__db
+        )
+
+    def selectSql(self,sql):
+        cur = self.__con.cursor()
+        cur.execute(sql)
+        res = cur.fetchall()
+        cur.close()
+        self.__reccnt = len(res)
+        return res
+
+    def excSql(self,sql):
+        cur=self.__con.cursor()
+        cur.execute(sql)
+        self.__con.commit()
+        cur.close()
